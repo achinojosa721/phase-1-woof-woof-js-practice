4bc3a39a6235a3adf163465e125aa2828118a2de
Alberto Hinojosa <ahinojosa721@gmail.com>
2022-07-14 18:42:48 -0500

    DOM selectors added

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const baseURL = " http://localhost:3000/pups"
+
+const bar = document.querySelector("#dog-bar")
+const details = document.querySelector("#dog-info")
+const filterButton = document.querySelector("#good-dog-filter")
