89c3cad4f887ecf03441a846f67660320f679fa2
Alberto Hinojosa <ahinojosa721@gmail.com>
2022-07-14 18:48:18 -0500

    fetch requests

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ const baseURL = " http://localhost:3000/pups"
 const bar = document.querySelector("#dog-bar")
 const details = document.querySelector("#dog-info")
 const filterButton = document.querySelector("#good-dog-filter")
+
+filterButton.addEventListener('click', toggleFilter)
+
+function getAllDogs(){
+    return fetch(baseURL)
+    .then (response => response.json())
+    .then(renderAllInBar)
+}
+
+function getOneDog(id){
+    return fetch(baseURL + `/${id}`)
+    .then(response => response.json())
+}
